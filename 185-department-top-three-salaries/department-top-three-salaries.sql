10876c9d607ee5aa9d3412895c50957b620c8e32
Vijay Khetre <vkhetre69@gmail.com>
2026-07-31 00:35:36 +0530

    Time: 1123 ms (40.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,12 @@
+select  Department ,Employee , Salary
+ from
+(select d.name as  Department,
+e.name as Employee ,
+e.salary as Salary ,
+dense_rank() over (partition by d.name 
+order by e.salary desc) as rnk
+from Employee e
+left join Department d
+on e.departmentId=d.id ) as temp
+where rnk<=3
+
