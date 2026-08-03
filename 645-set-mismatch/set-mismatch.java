bcc15b2fff3a126d5ff6602278c16510689f146e
Vijay Khetre <vkhetre69@gmail.com>
2026-08-03 07:54:31 +0530

    Time: 3 ms (66.90%) | Memory: 47.3 MB (94.71%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=index+1){
+                return new int[] {arr[index],index+1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
