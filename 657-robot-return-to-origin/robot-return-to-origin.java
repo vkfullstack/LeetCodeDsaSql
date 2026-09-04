28a664ddbadb23632da44fa391ce5eaf0bc12cc6
Vijay Khetre <vkhetre69@gmail.com>
2026-09-04 09:15:14 +0530

    Time: 6 ms (49.98%) | Memory: 45.5 MB (78.76%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+
+            char ch = moves.charAt(i);
+
+            if (ch == 'R') {
+                x++;
+            } else if (ch == 'L') {
+                x--;
+            } else if (ch == 'U') {
+                y++;
+            } else if (ch == 'D') {
+                y--;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
