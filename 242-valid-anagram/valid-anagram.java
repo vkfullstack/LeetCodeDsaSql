a173d7d34684682ae4c3528648c6bc65d522fff4
Vijay Khetre <vkhetre69@gmail.com>
2026-09-22 10:05:23 +0530

    Time: 7 ms (32.09%) | Memory: 44.4 MB (73.99%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //convert both to lowercase
+        s=s.toLowerCase();
+        t=t.toLowerCase();
+
+        // strip of all the white space
+        s=s.replace(" ","");
+        t=t.replace(" ","");
+        // initialize the buckets array
+        int [] counts = new int[26];
+        // fill the buckets
+        for(int i=0;i<s.length();i++){
+            counts[s.charAt(i)-'a']++;
+        }
+        // empty the buckets
+        for(int i=0;i<t.length();i++){
+            counts[t.charAt(i)-'a']--;
+        }
+
+        // check if all buckets are empty
+        for(int count : counts){
+            if(count!=0)
+            return false;
+        }
+        return true;
+    }
+}
