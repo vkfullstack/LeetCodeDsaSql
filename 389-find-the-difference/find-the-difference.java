01223b21e716d25f9bb14d493f12ed7e6d6dcd8e
Vijay Khetre <vkhetre69@gmail.com>
2026-09-23 16:45:57 +0530

    Time: 3 ms (42.99%) | Memory: 42.9 MB (84.32%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int [] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)-'a']--;
+        }
+       for (int i = 0; i < count.length; i++) {
+            if (count[i] == -1) {
+                return (char)('a' + i);
+            }
+        }
+        return ' ';
+    }
+}
